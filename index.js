8c2be6ac99028eb9fc9d70cb9ebe5156d37f1677
Prajay Tipre <prajaytipre@gmail.com>
2021-11-08 16:59:47 -0500

    commit tree af76e99215b6b419f7bde0593b5bb8c79958849d

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 
+function superbowlWin(array) {
+    let winEl = array.find(element => element.result === "W");
+    if (winEl) {
+        return winEl.year;
+    } else {
+        return undefined;
+    }
+}
